b2297fd35f6205bf35db5675d8907d1a66fa25aa
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-12-25 08:09:45 -0300

    finish

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.13
 
 require (
 	github.com/gin-gonic/gin v1.7.7
-	gorm.io/driver/postgres v1.2.3 // indirect
-	gorm.io/gorm v1.22.4 // indirect
+	gorm.io/driver/postgres v1.2.3
+	gorm.io/gorm v1.22.4
 )
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/hyperyuri/webapi-with-go/server"
+import (
+	"github.com/hyperyuri/webapi-with-go/database"
+	"github.com/hyperyuri/webapi-with-go/server"
+)
 
 func main() {
+	database.StartDB()
+
 	server := server.NewServer()
 
 	server.Run()
